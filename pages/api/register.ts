62d5d884823896692c95523de6d4412046c8adf8
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-05-23 09:32:03 +0530

    Minor bug fix

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -20,7 +20,7 @@ export default async function handler(
                 email,
                 username,
                 name,
-                password
+                hashedPassword
             }
         });
 
