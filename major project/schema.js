2706bb2ba9f98374d22c19bb152c85dc6ed10d3c
Avanthika Rajesh <avanthikamahe64@gmail.com>
2024-06-16 21:25:46 +0530

    backend

---
+++ major project/schema.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    'image.url': Joi.string().required(),  
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+  }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
+  }).required(),
+});
