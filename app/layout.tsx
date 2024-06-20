f882aefeb5af7e7a4b8834fd39b1a90141e45a81
nithin6x9 <nithinkrishnalal303@gmail.com>
2024-06-20 22:54:18 +0530

    Userinformation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body className={inter.className}>
         {/*Toaster*/}
 
-        <header>{/*Headder*/}</header>
+        <header>
+          <Header />
+        </header>
         <div>
             <main>{children}</main>
         </div>
